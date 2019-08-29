6d0054d300b9ca7e1cecf4ef3c720eed67533a65
yaboong <daniel.yebon.yoo@gmail.com>
2019-08-29 13:47:44 +0900

    Add: 테스트 프로필 타입세이프하게 사용할 수 있도록 TestProfile 추가

--- src/test/java/com/yaboong/alterbridge/TestProfile.java
+++ src/test/java/com/yaboong/alterbridge/TestProfile.java
@@ -4,5 +4,6 @@ package com.yaboong.alterbridge;
  * Created by yaboong on 2019-08-27
  */
 public interface TestProfile {
-    String Test = "test";
+    String TEST = "test";
+    String LOCAL = "local";
 }

083bf939b85e25a47abe5d0894df8984f79f3fee
yaboong <daniel.yebon.yoo@gmail.com>
2019-08-29 20:10:56 +0900

    Add: Querydsl 설정
    - (참고) https://jojoldu.tistory.com/372
    - implementation, annotationProcessor 등 gradle dependencies 추가할때 compile 로 변경해줘야함.
    - (정확한 이유는 잘 모르겠지만) lombok 등 compile 타임에 빌드되지 않는 라이브러리들이 포함되어 있으면 gradle compileQuerydsl 명령실행시 컴파일에러 발생함

---
+++ src/main/java/com/yaboong/alterbridge/config/querydsl/QuerydslConfiguration.java
@@ -0,0 +1,23 @@
+package com.yaboong.alterbridge.config.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Created by yaboong on 2019-08-29.
+ */
+@Configuration
+public class QuerydslConfiguration {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}

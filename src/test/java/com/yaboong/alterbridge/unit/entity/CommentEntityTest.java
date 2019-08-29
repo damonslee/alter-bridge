d530daf08d0344c41a1b8858d1c8d68227bec9ee
yaboong <daniel.yebon.yoo@gmail.com>
2019-08-29 20:15:06 +0900

    Add: Post, Comment 엔티티 CREATE 테스트코드 및 Querydsl 동작확인용 테스트코드 추가

--- src/test/java/com/yaboong/alterbridge/unit/entity/CommentEntityTest.java
+++ src/test/java/com/yaboong/alterbridge/unit/entity/CommentEntityTest.java
@@ -4,22 +4,26 @@ import com.yaboong.alterbridge.TestProfile;
 import com.yaboong.alterbridge.application.api.comment.Comment;
 import com.yaboong.alterbridge.application.api.comment.CommentRepository;
 import com.yaboong.alterbridge.application.api.post.Post;
-import com.yaboong.alterbridge.application.api.post.PostRepository;
+import com.yaboong.alterbridge.config.querydsl.QuerydslConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.hamcrest.Matchers.is;
+
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by yaboong on 2019-08-29.
  */
-@RunWith(SpringRunner.class)
 @DataJpaTest
+@RunWith(SpringRunner.class)
+@Import(QuerydslConfiguration.class)
 @ActiveProfiles(TestProfile.TEST)
 public class CommentEntityTest {
 
@@ -31,6 +35,7 @@ public class CommentEntityTest {
 
     @Test
     public void 댓글_등록() {
+        // GIVEN
         Post post = Post.builder()
                 .title("등록테스트입니다.")
                 .category("개발")
@@ -39,12 +44,42 @@ public class CommentEntityTest {
                 .build();
         testEntityManager.persist(post);
 
+        // WHEN
         Comment comment = Comment.builder()
                 .postId(post.getPostId())
                 .content("테스트 댓글")
                 .createdBy("oyabun")
                 .build();
         commentRepository.save(comment);
+
+        // THEN
         assertThat(commentRepository.getOne(comment.getCommentId()), is(comment));
+    }
+
+    @Test
+    public void querydslTest() {
+        // GIVEN
+        Post post = Post.builder()
+                .title("등록테스트입니다.")
+                .category("개발")
+                .content("내용입니다.")
+                .createdBy("yaboong")
+                .build();
+        testEntityManager.persist(post);
+
+        // WHEN
+        Comment comment = Comment.builder()
+                .postId(post.getPostId())
+                .content("TEST COMMENT")
+                .createdBy("tester")
+                .build();
+        testEntityManager.persist(comment);
+
+        // THEN
+        assertEquals(
+                "TEST COMMENT",
+                commentRepository.findByContent("TEST COMMENT").get(0).getContent()
+        );
+
     }
 }
--- src/test/java/com/yaboong/alterbridge/unit/entity/PostEntityTest.java
+++ src/test/java/com/yaboong/alterbridge/unit/entity/PostEntityTest.java
@@ -3,33 +3,38 @@ package com.yaboong.alterbridge.unit.entity;
 import com.yaboong.alterbridge.TestProfile;
 import com.yaboong.alterbridge.application.api.post.Post;
 import com.yaboong.alterbridge.application.api.post.PostRepository;
+import com.yaboong.alterbridge.config.querydsl.QuerydslConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 /**
  * Created by yaboong on 2019-08-29.
  */
-@RunWith(SpringRunner.class)
 @DataJpaTest
+@RunWith(SpringRunner.class)
+@Import(QuerydslConfiguration.class)
 @ActiveProfiles(TestProfile.TEST)
 public class PostEntityTest {
 
     @Autowired
-    TestEntityManager testEntityManager;
+    private TestEntityManager testEntityManager;
 
     @Autowired
-    PostRepository postRepository;
+    private PostRepository postRepository;
 
     @Test
     public void 게시글_등록() {
+        // GIVEN
         Post post = Post.builder()
                 .title("등록테스트입니다.")
                 .category("개발")
@@ -37,7 +42,31 @@ public class PostEntityTest {
                 .createdBy("yaboong")
                 .build();
 
-        postRepository.save(post);
-        assertThat(postRepository.getOne(post.getPostId()), is(post));
+        // WHEN
+        testEntityManager.persist(post);
+
+        Post savedPost = postRepository.getOne(post.getPostId());
+
+        // THEN
+        assertThat(savedPost, is(post));
+        assertEquals(savedPost.getTitle(), "등록테스트입니다.");
+    }
+
+    @Test
+    public void querydslTest() {
+        // GIVEN
+        Post post = Post.builder()
+                .title("등록테스트입니다.")
+                .category("개발")
+                .content("내용입니다.")
+                .createdBy("yaboong")
+                .build();
+
+        // WHEN
+        testEntityManager.persist(post);
+        Post savedPost = postRepository.findByTitle(post.getTitle()).get(0);
+
+        // THEN
+        assertThat(savedPost, is(post));
     }
 }

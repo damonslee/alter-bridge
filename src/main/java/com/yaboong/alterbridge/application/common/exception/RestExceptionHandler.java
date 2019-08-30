0bec78314877537c0b1dc91128820fe42adacb19
yaboong <daniel.yebon.yoo@gmail.com>
2019-08-31 02:07:14 +0900

    Add: 예외처리 핸들러 작성

---
+++ src/main/java/com/yaboong/alterbridge/application/common/exception/RestExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.yaboong.alterbridge.application.common.exception;
+
+import com.yaboong.alterbridge.application.common.response.ResponseBase;
+import com.yaboong.alterbridge.application.common.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by yaboong on 2019-08-30
+ */
+@Slf4j
+@ControllerAdvice
+public class RestExceptionHandler  {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseEntity handleException(Exception exception) {
+        return error(
+                ApiResponse.SOMETHING_WENT_WRONG,
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception
+        );
+    }
+
+    @ExceptionHandler(ApiException.class)
+    @ResponseBody
+    public ResponseEntity handleApiException(ApiException apiException) {
+        return error(
+                ApiResponse.UNKNOWN_ERROR,
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                apiException
+        );
+    }
+
+    private ResponseEntity error(ApiResponse apiResponse, HttpStatus status, Exception exception) {
+        log.error(exception.getMessage(), exception);
+        return ResponseEntity
+                .status(status)
+                .body(ResponseBase.of(apiResponse));
+    }
+}

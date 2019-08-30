944de73ed0f3df4f766c08dc46359126ee89c81a
yaboong <daniel.yebon.yoo@gmail.com>
2019-08-31 01:56:22 +0900

    Update: 인터셉터 아무것도 안하게 수정

--- src/main/java/com/yaboong/alterbridge/configuration/interceptor/KeywordInterceptor.java
+++ src/main/java/com/yaboong/alterbridge/configuration/interceptor/KeywordInterceptor.java
@@ -1,7 +1,6 @@
 package com.yaboong.alterbridge.configuration.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,17 +14,7 @@ public class KeywordInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        log.info("-> KeywordInterceptor preHandle");
         return true;
     }
 
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
-        log.info("-> KeywordInterceptor postHandle");
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object object, Exception arg3) {
-        log.info("-> KeywordInterceptor afterCompletion" );
-    }
 }

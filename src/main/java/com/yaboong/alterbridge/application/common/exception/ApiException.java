6aa4302b5f8b8d3370396d8bf09a1e1f4cf0e659
yaboong <daniel.yebon.yoo@gmail.com>
2019-08-31 02:06:59 +0900

    Add: 애플리케이션 내부적으로 사용할 공용 예외타입 지정

--- src/main/java/com/yaboong/alterbridge/application/common/exception/ApiException.java
+++ src/main/java/com/yaboong/alterbridge/application/common/exception/ApiException.java
@@ -1,13 +1,15 @@
 package com.yaboong.alterbridge.application.common.exception;
 
+import lombok.NoArgsConstructor;
+
 /**
  * Created by yaboong on 2019-08-30
  */
-public class ApiException extends RuntimeException {
-    public ApiException() {
-    }
+@NoArgsConstructor
+public class ApiException extends Exception {
 
     public ApiException(String message) {
         super(message);
     }
+
 }

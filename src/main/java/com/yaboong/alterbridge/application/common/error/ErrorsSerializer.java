e89ea742d12b3532425ed2b3da306f56fbfe2ece
yaboong <daniel.yebon.yoo@gmail.com>
2019-09-11 05:04:31 +0900

    Add: ErrorsSerializer 추가
    - Errors 오브젝트를 Response Body 에 JSON 형태로 변환하는 것이 Serialization
    - Errors 는 자바빈 스펙을 준수하고 있지 않으므로 직접 Serializer 를 구현해야 Serialize 할 수 있음
    - (참고) 자바빈 스펙을 준수하고 있는 EventDto 와 같은 경우는 BeanSerializer 를 통해 JSON 으로 변환됨

---
+++ src/main/java/com/yaboong/alterbridge/application/common/error/ErrorsSerializer.java
@@ -0,0 +1,52 @@
+package com.yaboong.alterbridge.application.common.error;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * Created by yaboong on 2019-09-11
+ *
+ * Validation 결과로 받은 Errors 오브젝트는 기본적으로 Serialization (JSON 변환) 이 불가능하므로,
+ * 직접 Serialization 방법을 정의해주고, @JsonComponent 애노테이션을 사용하여 ModelMapper 에 직접 등록해준다.
+ */
+@Slf4j
+@JsonComponent
+public class ErrorsSerializer extends JsonSerializer<Errors> {
+
+    /**
+     * Stream 사용시 lambda 함수 내에서 예외처리 해주어야 하므로 사용하지 않음
+     */
+    @Override
+    public void serialize(Errors errors, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeStartArray();
+        for (FieldError fieldError : errors.getFieldErrors()) {
+            gen.writeStartObject();
+            gen.writeStringField("field", fieldError.getField());
+            gen.writeStringField("code", fieldError.getCode());
+            gen.writeStringField("defaultMessage", fieldError.getDefaultMessage());
+            Object rejectedValue = fieldError.getRejectedValue();
+            if (Objects.nonNull(rejectedValue)) {
+                gen.writeStringField("rejectedValue", rejectedValue.toString());
+            }
+            gen.writeEndObject();
+        }
+
+        for (ObjectError error : errors.getGlobalErrors()) {
+            gen.writeStartObject();
+            gen.writeStringField("objectName", error.getObjectName());
+            gen.writeStringField("code", error.getCode());
+            gen.writeStringField("defaultMessage", error.getDefaultMessage());
+            gen.writeEndObject();
+        }
+        gen.writeEndArray();
+    }
+}

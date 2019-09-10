f3b8b216c54abe31057952fa2f63096e0e8de105
yaboong <daniel.yebon.yoo@gmail.com>
2019-09-11 05:07:41 +0900

    Change: 통합테스트용 껍데기

--- src/test/java/com/yaboong/alterbridge/AlterBridgeApplicationTests.java
+++ src/test/java/com/yaboong/alterbridge/AlterBridgeApplicationTests.java
@@ -1,7 +1,14 @@
 package com.yaboong.alterbridge;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ActiveProfiles(TestProfile.TEST)
 public class AlterBridgeApplicationTests {
 
     @Test

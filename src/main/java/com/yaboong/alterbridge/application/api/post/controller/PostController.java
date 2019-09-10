00237d2b549f3aab49e2b0a9bfc084db0a441a69
yaboong <daniel.yebon.yoo@gmail.com>
2019-09-11 05:05:02 +0900

    Add: PostController 껍데기 추가

--- src/main/java/com/yaboong/alterbridge/application/api/post/controller/PostController.java
+++ src/main/java/com/yaboong/alterbridge/application/api/post/controller/PostController.java
@@ -1,13 +1,48 @@
 package com.yaboong.alterbridge.application.api.post.controller;
 
+import com.yaboong.alterbridge.application.api.post.domain.PostDto;
+import com.yaboong.alterbridge.application.api.post.domain.PostValidator;
+import com.yaboong.alterbridge.application.common.response.ApiResponse;
+import com.yaboong.alterbridge.application.common.response.ResponseBase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by yaboong on 2019-09-11
  */
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/posts")
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostValidator postValidator;
+
+    @PostMapping
+    public ResponseEntity createPost(@RequestBody @Valid PostDto postDto, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(ResponseBase.of(ApiResponse.INVALID_REQUEST, errors));
+            // return ResponseEntityBuilder.badRequest(errors);
+        }
+
+        postValidator.validate(postDto, errors);
+        if (errors.hasErrors()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(ResponseBase.of(ApiResponse.INVALID_REQUEST, errors));
+        }
+
+        return ResponseEntity
+                .ok()
+                .body(ResponseBase.of(ApiResponse.OK));
+    }
 
 }

d7e2c47edf0d996dbd5d1bef7c3508a1d83d1171
yaboong <daniel.yebon.yoo@gmail.com>
2019-09-12 02:38:42 +0900

    Refactor: 클래스 위치변경

--- src/main/java/com/yaboong/alterbridge/application/api/post/entity/PostFile.java
+++ src/main/java/com/yaboong/alterbridge/application/api/post/entity/PostFile.java
@@ -1,5 +1,6 @@
-package com.yaboong.alterbridge.application.api.boardfile.entity;
+package com.yaboong.alterbridge.application.api.post.entity;
 
+import com.yaboong.alterbridge.application.api.boardfile.entity.BoardFile;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,10 +13,10 @@ import javax.persistence.Table;
  * Created by yaboong on 2019-09-11
  */
 @Entity
-@Table(name = "comment_file")
-@DiscriminatorValue("COMMENT")
+@Table(name = "post_file")
+@DiscriminatorValue("POST")
 @Getter
 @Setter
 @AllArgsConstructor
-public class CommentFile extends BoardFile {
+public class PostFile extends BoardFile {
 }

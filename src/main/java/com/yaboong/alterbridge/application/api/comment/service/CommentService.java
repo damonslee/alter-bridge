3bf2a80643c2e3be00e94d39a22b7a79b7e5358d
yaboong <daniel.yebon.yoo@gmail.com>
2019-09-13 01:06:29 +0900

    Remove: CommentService 의 get(id) 메서드 제거
    - comment 하나만 가지고 올 일은 없을 것 같다

--- src/main/java/com/yaboong/alterbridge/application/api/comment/service/CommentService.java
+++ src/main/java/com/yaboong/alterbridge/application/api/comment/service/CommentService.java
@@ -12,11 +12,9 @@ public interface CommentService {
 
     Comment create(Long postId, CommentDto dto);
 
-    Optional<Comment> modify(Long id, CommentDto dto);
+    Optional<Comment> modify(Long commentId, CommentDto dto);
 
-    Optional<Comment> softRemove(Long id);
-
-    Optional<Comment> get(Long id);
+    Optional<Comment> softRemove(Long commentId);
 
     void remove(Long id);
 

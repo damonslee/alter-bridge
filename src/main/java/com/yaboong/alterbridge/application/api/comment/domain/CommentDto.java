c78c1f4433597fd2ce61fb277cc87f627bfe41b0
yaboong <daniel.yebon.yoo@gmail.com>
2019-09-13 00:27:57 +0900

    Remove: CommentDto 에 postId 필드 제거
    - comment 등록할때 (RESTful 하게? ㅋㅋ) 하려면
    - [HTTP POST] /posts/{POST_ID}/comments
    - 이렇게 할텐데, 그러면 CommentDto 에 굳이 postId 를 가지고 있을 필요가 없지
    - 어쩐지 좀 찝찝하던데 역시 아직 갈길이 멀구나 이노무새끼야

--- src/main/java/com/yaboong/alterbridge/application/api/comment/domain/CommentDto.java
+++ src/main/java/com/yaboong/alterbridge/application/api/comment/domain/CommentDto.java
@@ -5,7 +5,6 @@ import lombok.experimental.FieldDefaults;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
 
@@ -18,9 +17,6 @@ import java.util.List;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CommentDto {
-
-    @NotEmpty @Positive
-    Long postId;
 
     @NotEmpty
     String content;

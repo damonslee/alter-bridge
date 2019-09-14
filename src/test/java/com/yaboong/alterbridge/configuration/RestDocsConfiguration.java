c9546e78ffe7403b9950d5b189e8820991c49f09
yaboong <daniel.yebon.yoo@gmail.com>
2019-09-15 03:14:44 +0900

    Add: RestDocs 적용 #1 - configuration 클래스 추가

---
+++ src/test/java/com/yaboong/alterbridge/configuration/RestDocsConfiguration.java
@@ -0,0 +1,20 @@
+package com.yaboong.alterbridge.configuration;
+
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+/**
+ * Created by yaboong on 2019-09-15
+ */
+@TestConfiguration
+public class RestDocsConfiguration {
+    @Bean
+    public RestDocsMockMvcConfigurationCustomizer restDocsMockMvcConfigurationCustomizer() {
+        return configurer -> configurer.operationPreprocessors()
+                .withRequestDefaults(prettyPrint())
+                .withResponseDefaults(prettyPrint());
+    }
+}

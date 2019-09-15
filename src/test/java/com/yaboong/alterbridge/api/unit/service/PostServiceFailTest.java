a239b3b9b37d2c31149539710c20834bff502122
yaboong <daniel.yebon.yoo@gmail.com>
2019-09-16 01:41:51 +0900

    Add: PostService 수정 실패 유닛테스트 추가 - Mockito never, any 사용
    - 서비스 로직이 너무 단순해서 딱히 테스트할 내용이 없음
    - 의존하고 있는 빈들의 호출 순서와 호출 횟수로 테스트

---
+++ src/test/java/com/yaboong/alterbridge/api/unit/service/PostServiceFailTest.java
@@ -0,0 +1,51 @@
+package com.yaboong.alterbridge.api.unit.service;
+
+import com.yaboong.alterbridge.application.api.post.entity.Post;
+import com.yaboong.alterbridge.application.api.post.repository.PostRepository;
+import com.yaboong.alterbridge.application.api.post.service.PostServiceImpl;
+import com.yaboong.alterbridge.common.TestProfile;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.modelmapper.ModelMapper;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by yaboong on 2019-09-16
+ */
+@RunWith(MockitoJUnitRunner.class)
+@ActiveProfiles(TestProfile.TEST)
+public class PostServiceFailTest {
+
+    @InjectMocks
+    PostServiceImpl postServiceImpl;
+
+    @Mock
+    PostRepository postRepository;
+
+    @Mock
+    ModelMapper modelMapper;
+
+    @Test
+    public void 게시물_수정_없는게시물조회_실패() {
+        // GIVEN
+        Long postId = 1L;
+        when(postRepository.findById(postId)).thenReturn(Optional.empty());
+
+        // WHEN
+        Optional<Post> postOptional = postServiceImpl.modify(postId, any());
+
+        // THEN
+        assertTrue(postOptional.isEmpty());
+        verify(modelMapper, never()).map(any(), any());
+        verify(postRepository, never()).save(any());
+    }
+}

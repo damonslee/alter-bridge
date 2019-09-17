ac14af442a6dade5303e42cf29ffff52f51e52e8
yaboong <daniel.yebon.yoo@gmail.com>
2019-09-18 01:56:29 +0900

    Fix: Fetch Join 으로 N+1 문제 해결
    - 기존에 엔티티 매핑시 fetch 옵션은 Post, Comment 모두 LAZY 로 해둠
    - Post 조회할때, JpaRepository 인터페이스가 기본으로 제공하는 findBy*And*() 형태로 사용했었는데 N+1 문제 발생
    - 쿼리가 아래 처럼 두개 날라감
    Hibernate:
        select
            post0_.post_id as post_id1_1_,
            post0_.created_at as created_2_1_,
            post0_.created_by as created_3_1_,
            post0_.modified_at as modified4_1_,
            post0_.modified_by as modified5_1_,
            post0_.category as category6_1_,
            post0_.content as content7_1_,
            post0_.like_count as like_cou8_1_,
            post0_.status as status9_1_,
            post0_.title as title10_1_,
            post0_.view_count as view_co11_1_
        from
            post post0_
        where
            post0_.post_id=?
            and post0_.status=?
    Hibernate:
        select
            comments0_.post_id as post_id9_0_0_,
            comments0_.comment_id as comment_1_0_0_,
            comments0_.comment_id as comment_1_0_1_,
            comments0_.created_at as created_2_0_1_,
            comments0_.created_by as created_3_0_1_,
            comments0_.modified_at as modified4_0_1_,
            comments0_.modified_by as modified5_0_1_,
            comments0_.content as content6_0_1_,
            comments0_.like_count as like_cou7_0_1_,
            comments0_.post_id as post_id9_0_1_,
            comments0_.status as status8_0_1_
        from
            comment comments0_
        where
            comments0_.post_id=?
    
    - Querydsl 로 직접 쿼리짬
    - 이때 fetchJoin() 적용
    - 결과 -> 쿼리 하나 날라감
    Hibernate:
        select
            post0_.post_id as post_id1_1_0_,
            comments1_.comment_id as comment_1_0_1_,
            post0_.created_at as created_2_1_0_,
            post0_.created_by as created_3_1_0_,
            post0_.modified_at as modified4_1_0_,
            post0_.modified_by as modified5_1_0_,
            post0_.category as category6_1_0_,
            post0_.content as content7_1_0_,
            post0_.like_count as like_cou8_1_0_,
            post0_.status as status9_1_0_,
            post0_.title as title10_1_0_,
            post0_.view_count as view_co11_1_0_,
            comments1_.created_at as created_2_0_1_,
            comments1_.created_by as created_3_0_1_,
            comments1_.modified_at as modified4_0_1_,
            comments1_.modified_by as modified5_0_1_,
            comments1_.content as content6_0_1_,
            comments1_.like_count as like_cou7_0_1_,
            comments1_.post_id as post_id9_0_1_,
            comments1_.status as status8_0_1_,
            comments1_.post_id as post_id9_0_0__,
            comments1_.comment_id as comment_1_0_0__
        from
            post post0_
        inner join
            comment comments1_
                on post0_.post_id=comments1_.post_id
        where
            post0_.post_id=?
            and post0_.status=?
    - 이때 querydsl 에서 join() 후 fetchJoin() 붙여주지 않으면 기존이랑 똑같이 N+1 발생. N+1 해결의 키는 fetchJoin() 인 것 같다.

--- src/main/java/com/yaboong/alterbridge/application/api/post/repository/PostRepositoryCustom.java
+++ src/main/java/com/yaboong/alterbridge/application/api/post/repository/PostRepositoryCustom.java
@@ -1,12 +1,17 @@
 package com.yaboong.alterbridge.application.api.post.repository;
 
 import com.yaboong.alterbridge.application.api.post.entity.Post;
+import com.yaboong.alterbridge.application.common.type.Status;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by yaboong on 2019-08-29.
  */
 public interface PostRepositoryCustom {
-    List<Post> findByTitle(String title);
+    Optional<Post> findPostAndCommentByPostId(Long postId, Status status);
+
+    List<Post> findAllPost(Status status);
+
 }

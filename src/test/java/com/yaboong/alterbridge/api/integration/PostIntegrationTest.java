945938fcfe9267ba274bbe480089f088395d2845
yaboong <daniel.yebon.yoo@gmail.com>
2019-09-19 01:36:59 +0900

    Change: post 조회시 comments 필드는 json ignore 했으므로 RestDocs 필드에서 제거

--- src/test/java/com/yaboong/alterbridge/api/integration/PostIntegrationTest.java
+++ src/test/java/com/yaboong/alterbridge/api/integration/PostIntegrationTest.java
@@ -99,7 +99,6 @@ public class PostIntegrationTest {
                             fieldWithPath("viewCount").description("게시물 조회수"),
                             fieldWithPath("likeCount").description("게시물 좋아요 수"),
                             fieldWithPath("status").description("게시물 상태"),
-                            fieldWithPath("comments").description("게시물에 달린 댓글 목록"),
                             fieldWithPath("_links.self.href").description("link to self"),
                             fieldWithPath("_links.self.type").description("http method for link to self"),
                             fieldWithPath("_links.update-post.href").description("link to update post"),

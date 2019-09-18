08bc34a52cd235e09d228600621095bfad4c9527
yaboong <daniel.yebon.yoo@gmail.com>
2019-09-19 01:43:03 +0900

    Add: Service 테스트 - 게시물 수정 성공
    - 서비스 클래스와 의존관계에 있지 않은 Post, PostDto 는 별도 mocking 해줘야함

--- src/test/java/com/yaboong/alterbridge/api/unit/service/PostServiceSuccessTest.java
+++ src/test/java/com/yaboong/alterbridge/api/unit/service/PostServiceSuccessTest.java
@@ -1,10 +1,11 @@
 package com.yaboong.alterbridge.api.unit.service;
 
 import com.yaboong.alterbridge.application.api.post.domain.PostDto;
+import com.yaboong.alterbridge.application.api.post.entity.Post;
 import com.yaboong.alterbridge.application.api.post.repository.PostRepository;
 import com.yaboong.alterbridge.application.api.post.service.PostServiceImpl;
-import com.yaboong.alterbridge.common.TestDataGenerator;
 import com.yaboong.alterbridge.common.TestProfile;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,9 +13,10 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
+
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by yaboong on 2019-09-16
@@ -29,16 +31,40 @@ public class PostServiceSuccessTest {
     @Mock
     PostRepository postRepository;
 
-    @Test
-    public void 게시물_생성_성공() {
-        // GIVEN
-        PostDto postDto = TestDataGenerator.newPostDto();
+    Post post;
 
+    PostDto postDto;
+
+    @Before
+    public void init() {
+        post = mock(Post.class);
+        postDto = mock(PostDto.class);
+    }
+
+
+    @Test
+    public void Service_게시물_생성_성공() {
         // WHEN
         postServiceImpl.create(postDto);
 
         // THEN
         verify(postRepository, times(1)).save(any());
+    }
+
+    @Test
+    public void Service_게시물_수정_성공() {
+        // GIVEN
+        long postId = 1L;
+        when(postRepository.findById(postId)).thenReturn(Optional.of(post));
+        when(post.apply(postDto)).thenReturn(post);
+
+        // WHEN
+        postServiceImpl.modify(postId, postDto);
+
+        // THEN
+        verify(postRepository, times(1)).findById(postId);
+        verify(post, times(1)).apply(postDto);
+        verify(postRepository, times(1)).save(post);
     }
 
 }

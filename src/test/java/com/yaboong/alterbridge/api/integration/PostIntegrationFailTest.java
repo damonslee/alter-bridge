2e20e64cac2aa4f253808251c77b80b221c11cbf
yaboong <daniel.yebon.yoo@gmail.com>
2019-09-27 23:19:41 +0900

    Change: 인자를 받을 수 있는 테스트는 통합테스트에 작성
    - 컨트롤러 단위테스트에 하려고 했는데, 컨트롤러 메소드 내부적으로 다른 빈을 많이 사용하고 있어서 mocking 할게 많아짐..
    - 코드를 잘못짰나?

--- src/test/java/com/yaboong/alterbridge/api/integration/PostIntegrationFailTest.java
+++ src/test/java/com/yaboong/alterbridge/api/integration/PostIntegrationFailTest.java
@@ -1,8 +1,15 @@
 package com.yaboong.alterbridge.api.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yaboong.alterbridge.application.api.post.domain.PostDto;
 import com.yaboong.alterbridge.application.api.post.repository.PostRepository;
+import com.yaboong.alterbridge.common.TestDescription;
 import com.yaboong.alterbridge.common.TestProfile;
 import com.yaboong.alterbridge.configuration.RestDocsConfiguration;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +19,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,13 +36,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * Created by yaboong on 2019-09-19
  */
-@RunWith(SpringRunner.class)
+@RunWith(JUnitParamsRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import({RestDocsConfiguration.class})
 @ActiveProfiles(TestProfile.TEST)
 public class PostIntegrationFailTest {
+
+    /**
+     * SpringClassRule, SpringMethodRule 이 @RunWith(SpringRunner.class) 를 대체함
+     * JUnit 에서 한번에 하나의 Runner 만 지원하기 때문에 JUnitParamRunner 도 사용하고, SpringRunner 도 사용하려면 일단 이렇게
+     */
+    @ClassRule
+    public static final SpringClassRule springClassRule = new SpringClassRule();
+
+    @Rule
+    public final SpringMethodRule springMethodRule = new SpringMethodRule();
 
     @Value("${spring.data.web.pageable.max-page-size}")
     Long MAX_PAGE_SIZE;
@@ -42,6 +62,9 @@ public class PostIntegrationFailTest {
 
     @Autowired
     PostRepository postRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
     public void PostController_Pageable_Size_제한() throws Exception {
@@ -56,8 +79,32 @@ public class PostIntegrationFailTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("page").exists())
-                .andExpect(jsonPath("page.size").value(MAX_PAGE_SIZE))
-        ;
+                .andExpect(jsonPath("page.size").value(MAX_PAGE_SIZE));
+    }
 
+
+    @Test
+    @TestDescription("잘못된 파라미터를 받는 경우 400 응답")
+    @Parameters(method = "inputParamsInvalidPostDto")
+    public void PostController_유효하지_않은_파라미터_400(PostDto postDto) throws Exception {
+        // GIVEN - by param
+        String postDtoJson = objectMapper.writeValueAsString(postDto);
+
+        // WHEN
+        MockHttpServletRequestBuilder request = post("/posts")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaTypes.HAL_JSON)
+                .content(postDtoJson);
+
+        // THEN
+        this.mockMvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    public Object inputParamsInvalidPostDto() {
+        return new Object[] {
+                PostDto.builder().status(null).title("").content("").viewCount(0L).likeCount(0L).category("GENERAL").build()
+        };
     }
 }

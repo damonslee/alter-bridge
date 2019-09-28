e31bdf35f68973aa8af11d174c2e7a38cf093687
yaboong <daniel.yebon.yoo@gmail.com>
2019-09-28 14:02:50 +0900

    Add Test: null 값을 포함하는 경우와 유효하지 않은 값을 가지는 경우 테스트 분리
    - 필드값이 null 로 오는 경우에는 응답에 rejectedValue 필드가 포함되지 않으므로 분리함

--- src/test/java/com/yaboong/alterbridge/api/integration/PostIntegrationFailTest.java
+++ src/test/java/com/yaboong/alterbridge/api/integration/PostIntegrationFailTest.java
@@ -2,7 +2,6 @@ package com.yaboong.alterbridge.api.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yaboong.alterbridge.application.api.post.domain.PostDto;
-import com.yaboong.alterbridge.application.api.post.repository.PostRepository;
 import com.yaboong.alterbridge.common.DataPostDto;
 import com.yaboong.alterbridge.common.TestDescription;
 import com.yaboong.alterbridge.common.TestProfile;
@@ -47,7 +46,11 @@ public class PostIntegrationFailTest {
 
     /**
      * SpringClassRule, SpringMethodRule 이 @RunWith(SpringRunner.class) 를 대체함
-     * JUnit 에서 한번에 하나의 Runner 만 지원하기 때문에 JUnitParamRunner 도 사용하고, SpringRunner 도 사용하려면 일단 이렇게
+     * JUnit 에서 한번에 하나의 Runner 만 지원하기 때문에 JUnitParamRunner 도 사용하고, SpringRunner 도 사용하려면 일단 이렇게 해야함
+     *
+     * 2019.09.28 시점
+     * JUnitParams runner must be top junit runner. Combining it with spring was impossible until now.
+     * https://github.com/Pragmatists/junitparams-spring-integration-example
      */
     @ClassRule
     public static final SpringClassRule springClassRule = new SpringClassRule();
@@ -60,9 +63,6 @@ public class PostIntegrationFailTest {
 
     @Autowired
     MockMvc mockMvc;
-
-    @Autowired
-    PostRepository postRepository;
 
     @Autowired
     ObjectMapper objectMapper;
@@ -85,8 +85,38 @@ public class PostIntegrationFailTest {
 
 
     @Test
-    @TestDescription("잘못된 파라미터를 받는 경우 400 응답")
-    @Parameters(method = "inputParamsInvalidPostDto")
+    @TestDescription(
+            "request body json 필드가 null 값을 가지는 경우" +
+            "응답에 rejectedValue 필드가 포함되지 않음"
+    )
+    @Parameters(method = "inputContainsNullField")
+    public void Null_필드를_가지는경우_400(PostDto postDto) throws Exception {
+        // GIVEN - by param
+        String postDtoJson = objectMapper.writeValueAsString(postDto);
+
+        // WHEN
+        MockHttpServletRequestBuilder request = post("/posts")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaTypes.HAL_JSON)
+                .content(postDtoJson);
+
+        // THEN
+        this.mockMvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("content[0].field").exists())
+                .andExpect(jsonPath("content[0].code").exists())
+                .andExpect(jsonPath("content[0].defaultMessage").exists())
+                .andExpect(jsonPath("_links.profile.href").exists())
+        ;
+    }
+
+    @Test
+    @TestDescription(
+            "request body json 필드가 유효하지 않은 값을 가지는 경우" +
+            "응답에 rejectedValue 필드가 포함됨"
+    )
+    @Parameters(method = "inputContainsInvalidFieldValue")
     public void 유효하지_않은_파라미터로_게시물생성_400(PostDto postDto) throws Exception {
         // GIVEN - by param
         String postDtoJson = objectMapper.writeValueAsString(postDto);
@@ -100,11 +130,37 @@ public class PostIntegrationFailTest {
         // THEN
         this.mockMvc.perform(request)
                 .andDo(print())
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("content[0].field").exists())
+                .andExpect(jsonPath("content[0].code").exists())
+                .andExpect(jsonPath("content[0].defaultMessage").exists())
+                .andExpect(jsonPath("content[0].rejectedValue").exists())
+                .andExpect(jsonPath("_links.profile.href").exists())
+        ;
     }
 
+    @Test
+    @TestDescription("없는 게시물을 조회하는 경우 404 NOT FOUND 응답")
+    public void 게시물_1개조회_404() throws Exception {
+        // GIVEN
+        Long postId = Long.MAX_VALUE;
 
-    public Object inputParamsInvalidPostDto() {
-        return DataPostDto.invalidPostDtoObjects();
+        // WHEN
+        MockHttpServletRequestBuilder request = get("/posts/{id}", postId)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaTypes.HAL_JSON_UTF8_VALUE);
+
+        // THEN
+        this.mockMvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    public Object inputContainsInvalidFieldValue() {
+        return DataPostDto.containsInvalidFieldValue();
+    }
+
+    public Object inputContainsNullField() {
+        return DataPostDto.containsNullParams();
     }
 }

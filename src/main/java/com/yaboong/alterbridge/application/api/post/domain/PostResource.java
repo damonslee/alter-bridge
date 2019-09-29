35fd5a46e264242b81c57316ebd73ca2550f53c5
yaboong <daniel.yebon.yoo@gmail.com>
2019-09-29 16:06:34 +0900

    Refactor: 링크 생성은 PostResource 에서 담당하도록 수정
    - 링크는 Resource 에 해당하는 것이이므로,
    - PostResource 클래스에서 링크를 편집할 수 있는 메서드를 제공해주고,
    - PostLink 쪽에서는 링크를 생성하는데 필요한 정보들을 넘겨주도록 함
    - 그게 각자 클래스의 역할에 맞는 방법인듯

--- src/main/java/com/yaboong/alterbridge/application/api/post/domain/PostResource.java
+++ src/main/java/com/yaboong/alterbridge/application/api/post/domain/PostResource.java
@@ -1,23 +1,57 @@
 package com.yaboong.alterbridge.application.api.post.domain;
 
+import com.yaboong.alterbridge.application.api.post.controller.PostController;
 import com.yaboong.alterbridge.application.api.post.entity.Post;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpMethod;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 /**
  * Created by yaboong on 2019-09-14
  */
 public class PostResource extends Resource<Post> {
+
+    private static final String PROFILE_LINK_PREFIX = "/docs/index.html#";
+
+    private final Post post;
+
     private PostResource(Post post, Link... links) {
         super(post, links);
+        this.post = post;
     }
 
     public static PostResource of(Post post) {
         return new PostResource(post);
     }
 
-    public PostResource addLink(Link link) {
-        add(link);
+    public static Link getProfileLink(String anchor) {
+        return new Link(PROFILE_LINK_PREFIX+ anchor).withRel("profile");
+    }
+
+    public PostResource profile(String anchor) {
+        add(new Link(PROFILE_LINK_PREFIX+ anchor).withRel("profile"));
+        return this;
+    }
+
+    public PostResource self(HttpMethod httpMethod) {
+        add(linkTo(PostController.class).withSelfRel().withType(httpMethod.name()));
+        return this;
+    }
+
+    public PostResource selfWithId(HttpMethod httpMethod) {
+        add(linkTo(PostController.class).slash(this.post.getPostId()).withSelfRel().withType(httpMethod.name()));
+        return this;
+    }
+
+    public PostResource rel(HttpMethod httpMethod, String rel) {
+        add(linkTo(PostController.class).withRel(rel).withType(httpMethod.name()));
+        return this;
+    }
+
+    public PostResource relWithId(HttpMethod httpMethod, String rel) {
+        add(linkTo(PostController.class).slash(this.post.getPostId()).withRel(rel).withType(httpMethod.name()));
         return this;
     }
 
--- src/main/java/com/yaboong/alterbridge/application/api/post/link/PostLink.java
+++ src/main/java/com/yaboong/alterbridge/application/api/post/link/PostLink.java
@@ -1,11 +1,9 @@
 package com.yaboong.alterbridge.application.api.post.link;
 
-import com.yaboong.alterbridge.application.api.post.controller.PostController;
 import com.yaboong.alterbridge.application.api.post.domain.PostResource;
 import com.yaboong.alterbridge.application.api.post.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.hateoas.Link;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.ResourceAssembler;
 import org.springframework.http.HttpMethod;
@@ -13,56 +11,58 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.function.Function;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-
 /**
  * Created by yaboong on 2019-09-28
  */
 public class PostLink {
 
-    public static PagedResources addLinksForPostPagedList(PagedResourcesAssembler<Post> pagedResourcesAssembler, Page<Post> postPagedList) {
-        PagedResources<PostResource> postResources = pagedResourcesAssembler.toResource(postPagedList, postPagingResourceAssembler);
-        postResources.add(new Link("/docs/index.html#resources-posts-list").withRel("profile"));
-        return postResources;
+    private PostLink() {}
+
+    public static PagedResources addLinksForPostPagedList(
+            PagedResourcesAssembler<Post> pagedResourcesAssembler,
+            Page<Post> postPagedList
+    ) {
+        PagedResources<PostResource> postPagedResources = pagedResourcesAssembler.toResource(postPagedList, postPagingResourceAssembler);
+        postPagedResources.add(PostResource.getProfileLink("resources-query-posts"));
+        return postPagedResources;
     }
 
     public static PostResource addLinksForCreatePost(Post post) {
         return PostResource.of(post)
-                .addLink(linkTo(PostController.class).withSelfRel().withType(HttpMethod.POST.name()))
-                .addLink(new Link("/docs/index.html#resources-create-post").withRel("profile"))
-                .addLink(linkTo(PostController.class).withRel("post-list").withType(HttpMethod.GET.name()));
+                .self(HttpMethod.POST)
+                .profile("resources-create-post")
+                .rel(HttpMethod.GET, "post-list");
     }
 
     public static Function<Post, ResponseEntity> addLinksForUpdatePost =
             post -> ResponseEntity.ok(
                         PostResource.of(post)
-                            .addLink(linkTo(PostController.class).slash(post.getPostId()).withSelfRel().withType(HttpMethod.PUT.name()))
-                            .addLink(new Link("/docs/index.html#resources-update-post").withRel("profile"))
-                            .addLink(linkTo(PostController.class).withRel("post-list").withType(HttpMethod.GET.name()))
+                            .selfWithId(HttpMethod.PUT)
+                            .profile("resources-update-post")
+                            .rel(HttpMethod.GET, "post-list")
                         );
 
     public static Function<Post, ResponseEntity> addLinksForGetPost =
             post -> ResponseEntity.ok(
                         PostResource.of(post)
-                            .addLink(linkTo(PostController.class).slash(post.getPostId()).withSelfRel().withType(HttpMethod.GET.name()))
-                            .addLink(linkTo(PostController.class).slash(post.getPostId()).withRel("update-post").withType(HttpMethod.PUT.name()))
-                            .addLink(linkTo(PostController.class).slash(post.getPostId()).withRel("delete-post").withType(HttpMethod.DELETE.name()))
-                            .addLink(new Link("/docs/index.html#resources-posts-get").withRel("profile"))
+                            .selfWithId(HttpMethod.GET)
+                            .relWithId(HttpMethod.PUT, "update-post")
+                            .relWithId(HttpMethod.DELETE, "delete-post")
+                            .profile("resources-posts-get")
                         );
 
     public static Function<Post, ResponseEntity> addLinksForDeletePost =
             post -> ResponseEntity.ok(
                         PostResource.of(post)
-                            .addLink(linkTo(PostController.class).slash(post.getPostId()).withSelfRel().withType(HttpMethod.DELETE.name()))
-                            .addLink(linkTo(PostController.class).withRel("post-list").withType(HttpMethod.GET.name()))
-                            .addLink(new Link("/docs/index.html#resources-delete-post").withRel("profile"))
+                            .selfWithId(HttpMethod.DELETE)
+                            .rel(HttpMethod.GET, "post-list")
+                            .profile("resources-delete-post")
                         );
 
     private static ResourceAssembler<Post, PostResource> postPagingResourceAssembler =
             post -> PostResource.of(post)
-                        .addLink(new Link("/docs/index.html#resources-posts-get").withRel("profile"))
-                        .addLink(linkTo(PostController.class).slash(post.getPostId()).withSelfRel().withType(HttpMethod.GET.name()))
-                        .addLink(linkTo(PostController.class).slash(post.getPostId()).withRel("update-post").withType(HttpMethod.PUT.name()))
-                        .addLink(linkTo(PostController.class).slash(post.getPostId()).withRel("delete-post").withType(HttpMethod.DELETE.name()));
-
+                        .profile("resource-posts-get")
+                        .selfWithId(HttpMethod.GET)
+                        .relWithId(HttpMethod.PUT, "update-post")
+                        .relWithId(HttpMethod.DELETE, "delete-post");
 }
